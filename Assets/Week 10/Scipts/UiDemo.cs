fddbf28baba370b45c680de529b44920986aa950
Ronit0o0 <rathroni@sheridancollege.ca>
2024-03-21 15:11:27 -0400

    wekk10 task 2 completed

--- Assets/Week 10/Scipts/UiDemo.cs
+++ Assets/Week 10/Scipts/UiDemo.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIDemo : MonoBehaviour
 {
+    public TMP_Dropdown dropdown;
     public SpriteRenderer sr;
     public Color start;
     public Color end;
@@ -14,8 +16,12 @@ public class UIDemo : MonoBehaviour
         interpolation = value;
     }
 
+    public void DropDownHasChanged(int value)
+    {
+        Debug.Log(dropdown.options[value].text);
+    }
     private void Update()
     {
-        sr.color = Color.Lerp(start, end, interpolation/60);
+        sr.color = Color.Lerp(start, end, (interpolation/60));
     }
 }
